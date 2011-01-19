f3ff9c684bfdc3499969be9d02b367e12553ac50
dan rassi <dan.rassi@gmail.com>
2011-01-18 22:30:41 -0500

    small registration fixes

--- public/static/show-invite.js
+++ public/static/show-invite.js
@@ -1,6 +1,6 @@
 $(function () {
     var code = $('a').text();
-    var location = 'http://' + window.location.host + '/register/' + code;
+    var location = window.location.protocol + '//' + window.location.host + '/register/' + code;
     $('a').attr('href', location).text(location);
 });
 
